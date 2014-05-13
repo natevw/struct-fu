59d81c582f679c88613c118c509edcd3705788f0
Nathan Vander Wilt <nate@calftrail.com>
2014-05-13 13:40:56 -0700

    much better!

--- lib.js
+++ lib.js
@@ -1,65 +1,30 @@
 var _ = {};
 
-function old_field(name, count) {
-    var size = this.valueOf();
-    if (count == null) count = 1;
-    return function (struct) {
-        return {key:name, value:[struct._size,size*count]};
-    };
-};
-
-function extend(obj) {
-    Array.prototype.slice.call(arguments, 1).forEach(function (ext) {
-        Object.keys(ext).forEach(function (key) {
-            obj[key] = ext[key];
-        });
-    });
-    return obj;
-}
-
-function _normalizeFieldSig(read, type, dump, size) {
-    return {
-        valueFromBytes: function (buf, opts) {
-            var off = (opts && opts.offset) || 0,
-                obj = (opts && opts.destination) || new type();
-            read(buf, off, obj);
-            return obj;
+function arrayizeField(f, count) {
+    return (count) ? {
+        valueFromBytes: function (buf) {
+            var arr = new Array(count),
+                off = 0;
+            for (var idx = 0, len = arr.length; idx < len; idx += 1) {
+                var bytes = buf.slice(off, off += f.size),
+                    value = f.valueFromBytes(bytes);
+                arr[idx] = value;
+            }
+            return arr;
         },
-        bytesFromValue: function (obj, opts) {
-            var buf = (opts && opts.output) || new Buffer(size),
-                off = (opts && opts.offset) || 0;
-            dump(obj, buf, off);
-            return buff;
+        bytesFromValue: function (arr, buf) {
+            buf || (buf = new Buffer(this.size));
+            var off = 0;
+            for (var idx = 0, len = arr.length; idx < len; idx += 1) {
+                var value = arr[idx],
+                    bytes = buf.slice(off, off += f.size);
+                f.bytesFromValue(value, bytes);
+            }
+            return buf;
         },
-        size: size
-    };
-}
-
-function field(name, count) {
-    if (arguments.length < 2) {
-        count = name;
-        name = null;
-    }
-    var ctx = this;
-    return (count) ? _normalizeFieldSig(function (buf, off, arr) {
-        // read array
-        for (var i = 0; i < count; i += 1) {
-            arr[i] = ctx.read(buf, off);
-            off += ctx.size;
-        }
-    }, Array, function (arr, buf, off) {
-        // dump array
-        arr.forEach(function () {
-            ctx.dump(obj[name], buf, off);
-            off += ctx.size;
-        });
-    }, ctx.size*count) : _normalizeFieldSig(function (buf, off, obj) {
-        // read value
-        obj[name] = ctx.read(buff, off);
-    }, Object, function (obj, buf, off) {
-        // dump value
-        ctx.dump(obj[name], buf, off);
-    }, ctx.size);
+        size: f.size * count,
+        name: f.name
+    } : f;
 }
 
 _.struct = function (name, fields, count) {
@@ -68,59 +33,96 @@ _.struct = function (name, fields, count) {
         fields = name;
         name = null;
     }
-    return field.call({
-        read: function (buf, off) {
-            var obj = new Object();
-            fields.forEach(function (field) {
-                field.valueFromBytes(buf, {offset:off, destination:obj});
-                off += field.size;
+    return arrayizeField({
+        valueFromBytes: function (buf) {
+            var obj = new Object(),
+                off = 0;
+            fields.forEach(function (f) {
+                var bytes = buf.slice(off, off += f.size),
+                    value = f.valueFromBytes(bytes);
+                obj[f.name] = value;
             });
             return obj;
         },
-        dump: function (val, buf, off) {
-            fields.forEach(function (field) {
-            // TODO: val[name] stuff here instead, using `field.name`?
-                field.bytesFromValue(val, {offset:off, output:buf});
-                off += field.size;
+        bytesFromValue: function (obj, buf) {
+            buf || (buf = new Buffer(this.size));
+            var off = 0;
+            fields.forEach(function (f) {
+                var value = obj[f.name],
+                    bytes = buf.slice(off, off += f.size);
+                f.bytesFromValue(value, bytes);
             });
+            return buf;
         },
         size: fields.reduce(function (sum,field) {
             return sum + field.size;
         }, 0),
-    }, name, count);
-};
-
-_.byte = function (name, count) {
-    return _normalizeFieldSig(function _read(buf, off, obj) {
-        obj[name] = buff.slice(off, count);
-    }, Object, function _dump(obj, buf, off) {
-        obj[name].copy(buf, off, 0, count);
+        name: name
     }, count);
 };
 
-_.char = function (name, count) {
-    return _normalizeFieldSig(function _read(buf, off, obj) {
-        obj[name] = buff.slice(off, count).toString();
-    }, Object, function _dump(obj, buf, off) {
-console.log("char(dump)", obj, buf, off, name, count);
-        buf.write(obj[name], off, count);
+_.byte = function (name, size, count) {
+    if (typeof name !== 'string') {
+        count = size;
+        size = name;
+        name = null;
+    }
+    return arrayizeField({
+        valueFromBytes: function (buf) {
+            return buf;
+        },
+        bytesFromValue: function (val, buf) {
+            buf || (buf = new Buffer(this.size));
+            val.copy(buf, 0, 0, this.size)
+            return buf;
+        },
+        size: size,
+        name: name
     }, count);
 };
 
+_.char = function (name, size, count) {
+    if (typeof name !== 'string') {
+        count = size;
+        size = name;
+        name = null;
+    }
+    return arrayizeField({
+        valueFromBytes: function (buf) {
+            return buf.toString();
+        },
+        bytesFromValue: function (str, buf) {
+            buf || (buf = new Buffer(this.size));
+            buf.write(str, 0, this.size);
+            return buf;
+        },
+        size: size,
+        name: name
+    }, count);
+};
 
 function standardField(sig, size) {
     var read = 'read'+sig,
         dump = 'write'+sig;
     size || (size = +sig.match(/\d+/)[0] >> 3);
-    return field.bind({
-        read: function (buf, off) {
-            return buf[read](off);
-        },
-        dump: function (buf, off, val) {
-            buf[dump](val, off);
-        },
-        size: size
-    })
+    return function (name, count) {
+        if (typeof name !== 'string') {
+            count = name;
+            name = null;
+        }
+        return arrayizeField({
+            valueFromBytes: function (buf) {
+                return buf[read](0);
+            },
+            bytesFromValue: function (str, buf) {
+                buf || (buf = new Buffer(this.size));
+                buf[dump](val, 0);
+                return buf;
+            },
+            size: size,
+            name: name
+        }, count);
+    };
 }
 
 _.float32 = standardField('FloatBE',4);
@@ -148,3 +150,17 @@ _.sbit = standardField('Int8');
 //_.padTo?
 
 module.exports = _;
+
+
+
+
+
+
+function extend(obj) {
+    Array.prototype.slice.call(arguments, 1).forEach(function (ext) {
+        Object.keys(ext).forEach(function (key) {
+            obj[key] = ext[key];
+        });
+    });
+    return obj;
+}
