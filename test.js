28d628c8d1dcd354409570700c28363e32306daa
Nathan Vander Wilt <nate@calftrail.com>
2014-05-27 15:11:23 -0700

    add tests for hoisted field information

--- test.js
+++ test.js
@@ -12,7 +12,11 @@ var entry = _.struct([
         _.bool('archive'),
         _.ubit('reserved', 2)
     ].reverse()),
-    _.byte('reserved', 5, 2),
+    _.byte('reserved', 4, 2),
+    _.struct([
+        _.char('reserved1'),
+        _.ubit('reserved2', 8)
+    ]),
     _.struct('time', [
         _.ubit('hour',5),
         _.ubit('minutes',6),
@@ -38,11 +42,14 @@ function assert(b,msg) { if (!b) throw Error("Assertion failure. "+msg); else co
 console.log("  = API check =  ");
 assert('fields' in entry, "Entry has fields property.");
 assert('reserved' in entry.fields, "Entry fields contain 'reserved' bytefield.");
+assert('reserved1' in entry.fields, "Entry fields contain hoisted 'reserved1' bytefield.");
+assert('reserved2' in entry.fields, "Entry fields contain hoisted 'reserved2' bytefield.");
 assert('time' in entry.fields, "Entry fields contain 'time' struct.");
 assert(entry.fields.time.offset === 22, "Offset is correct for 'time' struct.");
 assert('field' in entry.fields.reserved, "Reserved array allows access to underlying field");
 assert(!('offset' in entry.fields.reserved.field), "Reserved array's underlying field does not have an offset…");
 assert(entry.fields.reserved.offset === 12, "…but reserved array field itself does, and said offset is correct.");
+assert(entry.fields.reserved2.offset.bytes === 21, "Hoisted field 'reserved2' has correct offset.");
 console.log("  = Write check =  ");
 var _bufKnown = new Buffer("6175746f65786563626174a00000000000000000000000000000000000000000", 'hex');
 assert(_bufKnown.length === 32, "Runtime parsed known buffer as 'hex'.");
